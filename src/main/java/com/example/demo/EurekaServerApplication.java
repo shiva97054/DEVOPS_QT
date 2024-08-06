13af3c2878fe21c5e60a361d792a9c2275c517df
chandrashekhar <chandranitu@gmail.com>
2024-08-06 12:29:19 +0530

    akjsbdjaskf

--- src/main/java/com/example/demo/EurekaServerApplication.java
+++ src/main/java/com/example/demo/EurekaServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.SpringApplication;
 
 public class EurekaServerApplication {
 	//private static final Logger logger = LoggerFactory.getLogger(EurekaServerApplication.class);
-
+        // chandra changed on 06-Aug-2024
 	public static void main(String[] args) {
 		//logger.info("Example log from {}", EurekaServerApplication.class.getSimpleName());
 		SpringApplication.run(EurekaServerApplication.class, args);
